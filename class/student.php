a13c14a04794ab2f1fb5cae14503bdb988700d68
DaniilLysenko <drovk199995@gmail.com>
2018-07-09 13:30:27 +0300

    added API methods for insert delete and find student

--- class/student.php
+++ class/student.php
@@ -111,4 +111,55 @@
 			}	
 			return false;
 		}
+
+		// API methods
+
+		public function addStudentAPI($name, $age, $sex, $phone, $birthday) 
+		{
+			$db = Db::getConnection();
+			try {
+				$sql = $db->prepare("INSERT INTO `student` (`name`, `age`, `sex`, `phone`, `birthday`) VALUES (:name, :age, :sex, :phone, :birthday)");
+				$sql->execute(['name' => $name,'age' => $age,'sex' => $sex,'phone' => $phone,'birthday' => $birthday]);
+				return json_encode(["student" => $this->getSingleStudent($db->lastInsertId())]);
+			} catch (PDOException $e) {
+				return json_encode(["error" => "Something wrong with db connection"]);
+			}
+		}
+
+		public function removeStudentAPI($id) 
+		{
+			$db = Db::getConnection();
+			try {
+				if (intval($id)) {
+					$sql = $db->prepare("DELETE FROM `student` WHERE `id` = :id");
+					$sql->execute(['id' => $id]);
+					return json_encode(["success" => "User removed successfully"]);
+				} else {
+					return json_encode(["error" => "Id is not a number"]);
+				}
+			} catch (PDOException $e) {
+				return json_encode(["error" => "Something wrong with db connection"]);
+			}
+		}
+
+		public function getAllStudentsAPI($page)
+		{
+			$db = Db::getConnection();
+			$offset = $page > 1 ? (($page - 1) * 3) : 0;
+			$sql = $db->prepare("SELECT * FROM `student` ORDER BY `id` DESC LIMIT 3 OFFSET ?");
+			$sql->bindValue(1, $offset, PDO::PARAM_INT);
+			$sql->execute();
+			return json_encode(["students" => $sql->fetchAll()]);
+		}
+
+		public function getSingleStudentAPI($id) 
+		{
+			if (intval($id)) {
+				$db = Db::getConnection();
+				$st = $db->prepare("SELECT * FROM `student` WHERE `id` = :id");
+				$st->execute(["id" => $id]);
+				return json_encode(["student" => $st->fetch(PDO::FETCH_LAZY)]);			
+			}
+			return json_encode(["error" => "Id is not a number"]);
+		}
 	}
